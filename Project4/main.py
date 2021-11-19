27b05fe37cbf1f952a96438e4d6a224594f2968d
AmirReza Azartoos <lio.azartoos@gmail.com>
2021-11-19 23:11:55 +0330

    chapter4 home work

---
+++ Project4/main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import roc_auc_score
+import numpy as np
+
+n_rows = 300000
+df = pd.read_csv("train.csv", nrows=n_rows)
+print(df.head(5))
+
+Y = df['click'].values
+X = df.drop(['click', 'id', 'hour', 'device_id', 'device_ip'], axis=1).values
+print(X.shape)
+
+n_train = int(n_rows * 0.9)
+X_train = X[:n_train]
+Y_train = Y[:n_train]
+X_test = X[n_train:]
+Y_test = Y[n_train:]
+
+enc = OneHotEncoder(handle_unknown='ignore')
+X_train_enc = enc.fit_transform(X_train)
+X_train_enc[0]
+print('X_train_enc', X_train_enc[0])
+
+X_test_enc = enc.transform(X_test)
+
+parameters = {'max_depth': [3, 10, None]}
+
+decision_tree = DecisionTreeClassifier(criterion='gini', min_samples_split=30)
+grid_search = GridSearchCV(decision_tree, parameters, n_jobs=-1, cv=3, scoring='roc_auc')
+grid_search.fit(X_train_enc, Y_train)
+print(grid_search.best_params_)
+
+best_params_ = {'max_depth': [10]}
+decision_tree_best = GridSearchCV(decision_tree, best_params_, n_jobs=-1, cv=3, scoring='roc_auc')
+decision_tree_best.fit(X_train_enc, Y_train)
+pos_prob = decision_tree_best.predict_proba(X_test_enc)[:, 1]
+print(f'The ROC AUC on testing set is: {roc_auc_score(Y_test, pos_prob):.3f}')
+
+pos_prob = np.zeros(len(Y_test))
+click_index = np.random.choice(len(Y_test), int(len(Y_test) * 51211.0/300000), replace=False)
+pos_prob[click_index] = 1
+print(f'The ROC AUC on testing set is: {roc_auc_score(Y_test, pos_prob):.3f}')
+
